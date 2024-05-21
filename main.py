f8fd18b2e3b6829977619d3f37d7b1c65c720943
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-05-22 00:34:58 +0530

    "Updated airport and flight nodes with additional attributes, and added attributes to edges representing flights."

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import networkx as nx
 # Create a directed graph
 G = nx.DiGraph()
 
-# Add nodes (airports)
-G.add_node("Airport_A")
-G.add_node("Airport_B")
-G.add_node("Airport_C")
+# Add airports with attributes
+G.add_node("Airport_A", location="City_A", runway_capacity=10, operating_hours="08:00-20:00")
+G.add_node("Airport_B", location="City_B", runway_capacity=8, operating_hours="07:00-19:00")
+G.add_node("Airport_C", location="City_C", runway_capacity=12, operating_hours="06:00-22:00")
 
-# Add edges (flights) with capacities
-G.add_edge("Airport_A", "Airport_B", capacity=100)
-G.add_edge("Airport_A", "Airport_C", capacity=150)
-G.add_edge("Airport_B", "Airport_C", capacity=200)
+# Add flights with attributes
+G.add_edge("Airport_A", "Airport_B", capacity=100, departure_time="08:30", arrival_time="10:30", duration=2)
+G.add_edge("Airport_A", "Airport_C", capacity=150, departure_time="09:00", arrival_time="12:00", duration=3)
+G.add_edge("Airport_B", "Airport_C", capacity=200, departure_time="10:00", arrival_time="13:00", duration=3)
 
-# Find the maximum flow
-max_flow_value = nx.maximum_flow_value(G, "Airport_A", "Airport_C", capacity='capacity')
-print("Maximum Flow Value:", max_flow_value)
+# Display airport and flight attributes
+for node in G.nodes(data=True):
+    print("Airport:", node)
+
+for edge in G.edges(data=True):
+    print("Flight:", edge)
